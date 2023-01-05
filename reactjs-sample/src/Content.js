fa41998c2d0ed49e7fb17fec878ed5221154c475
Chinthaka Bandara <chinthakapb@gmail.com>
2023-01-05 14:07:50 +0530

    React Click Events

--- reactjs-sample/src/Content.js
+++ reactjs-sample/src/Content.js
@@ -7,9 +7,34 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
       <p>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click here 1</button>
+      <button
+        onClick={() => {
+          handleClick2("Chinthaka");
+        }}
+      >
+        Click here 2
+      </button>
+      <button
+        onClick={(e) => {
+          handleClick3(e);
+        }}
+      >
+        Click here 3
+      </button>
     </main>
   );
 };
